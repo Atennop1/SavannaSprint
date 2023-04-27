114bfbea5cba5fb2d6cde63cab68d7237f0f6226
AtennopVer <anton7185569@gmail.com>
2023-04-27 22:04:32 +0300

    Changed privacy policy

--- Assets/Scripts/Menu/AgeConfirm.cs
+++ Assets/Scripts/Menu/AgeConfirm.cs
@@ -11,11 +11,31 @@ public class AgeConfirm : MonoBehaviour
     
     public void Click()
     {
+<<<<<<< HEAD
         selectSource.Play();
         if (input.text != "")
         {
             int age = int.Parse(input.text);
             PlayerPrefs.SetInt("age", age);
+=======
+        _selectSource.Play();
+        if (_ageField.text == "") 
+            return;
+        
+        var age = int.Parse(_ageField.text);
+        PlayerPrefs.SetInt("age", age);
+        gameObject.SetActive(false);
+    }
+
+    public void OpenPrivacyPolicy()
+    {
+        Application.OpenURL("https://doc-hosting.flycricket.io/savanna-sprint-two-worlds-privacy-policy/69a693a6-78ef-4332-9b77-a35defafbc14/privacy");
+    }
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("age"))
+>>>>>>> cee218e (Changed privacy policy)
             gameObject.SetActive(false);
         }
     }
